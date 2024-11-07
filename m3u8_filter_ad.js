19b7192a306112e1a197c2885cb512dd8e6dfa20
ltxlong <416803647@qq.com>
2024-11-07 15:18:55 +0800

    Update m3u8_filter_ad.js

--- m3u8_filter_ad.js
+++ m3u8_filter_ad.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              M3U8 Filter Ad Script
 // @namespace         http://tampermonkey.net/
-// @version           1.2.1
+// @version           1.3.0
 // @description       自用，拦截和过滤 m3u8（解析/采集资源） 的切片（插播）广告，同时在console打印过滤的行信息，不会误删。
 // @author            ltxlong
 // @match             *://*/*
@@ -16,8 +16,8 @@
 // @resource Swal     https://unpkg.com/sweetalert2@11/dist/sweetalert2.min.css
 // @resource SwalDark https://unpkg.com/@sweetalert2/theme-dark@5/dark.min.css
 // @license           MIT
-// @downloadURL https://update.greasyfork.org/scripts/512300/M3U8%20Filter%20Ad%20Script.user.js
-// @updateURL https://update.greasyfork.org/scripts/512300/M3U8%20Filter%20Ad%20Script.meta.js
+// @downloadURL       https://update.greasyfork.org/scripts/512300/M3U8%20Filter%20Ad%20Script.user.js
+// @updateURL         https://update.greasyfork.org/scripts/512300/M3U8%20Filter%20Ad%20Script.meta.js
 // ==/UserScript==
 
 (function() {
@@ -39,7 +39,7 @@
 
     let first_ts_name_index = -1; // 首个ts序列号
 
-    let ts_type = 0; // 0：xxxx000数字递增.ts模式0 ；1：xxxxxxxxxx.ts模式1；2：.ts模式2只能暴力拆解
+    let ts_type = 0; // 0：xxxx000数字递增.ts模式0 ；1：xxxxxxxxxx.ts模式1 ；2：***.ts模式2-暴力拆解
 
     let the_ext_x_mode = 0; // 0：ext_x_discontinuity判断模式0 ；1：ext_x_discontinuity判断模式1
 
@@ -53,8 +53,23 @@
 
     let violent_filter_mode_flag = false; // 是否启用暴力拆解模式，默认否-自动判断模式
 
+    let filter_log_html = '';
+
+    let filter_done_flag = false;
+
     function filter_log(...msg) {
+
+        const log_content = msg.join('</p><p>');
+
         console.log('%c[m3u8_filter_ad]', 'font-weight: bold; color: white; background-color: #70b566b0; padding: 2px; border-radius: 2px;', ...msg);
+
+        filter_log_html += `
+            <p>
+                <span style="font-weight: bold; color: white; background-color: #70b566b0; padding: 2px; border-radius: 2px;">[m3u8_filter_ad]</span>
+                ${log_content}
+            </p>
+       `;
+
     }
 
     let the_swalcss_color = "#ff679a";
@@ -80,61 +95,63 @@
 			`;
 
     // 动态添加样式
-    let try_add_style_n = 0;
-    function addStyle(id, css) {
+    function add_style(id, css) {
 
-        let tryToAddStyle = function() {
-            let styleDom = unsafeWindow.document.getElementById(id);
-            if (styleDom) styleDom.remove();
+        let try_add_style_n = 0;
 
-            let style = unsafeWindow.document.createElement('style');
-            style.rel = 'stylesheet';
-            style.id = id;
-            style.innerHTML = css;
+        let try_to_add_style = function() {
+            let the_style_dom = unsafeWindow.document.getElementById(id);
+            if (the_style_dom) the_style_dom.remove();
 
-            let targetElement = unsafeWindow.document.body;
-            if (targetElement) {
-                targetElement.insertBefore(style, targetElement.firstChild);
+            let the_style = unsafeWindow.document.createElement('style');
+            the_style.rel = 'stylesheet';
+            the_style.id = id;
+            the_style.innerHTML = css;
+
+            let the_target_element = unsafeWindow.document.body;
+            if (the_target_element) {
+                the_target_element.insertBefore(the_style, the_target_element.firstChild);
             } else {
                 try_add_style_n++;
                 if (try_add_style_n < 50) {
-                    setTimeout(tryToAddStyle, 100);
+                    setTimeout(try_to_add_style, 100);
                 }
             }
         };
 
-        tryToAddStyle();
+        try_to_add_style();
     }
 
     // 先监听颜色方案变化
     unsafeWindow.matchMedia('(prefers-color-scheme: dark)').addListener(function (e) {
         if (e.matches) {
             // 切换到暗色主题
-            addStyle('swal-pub-style', GM_getResourceText('SwalDark'));
+            add_style('swal-pub-style', GM_getResourceText('SwalDark'));
         } else {
             // 切换到浅色主题
-            addStyle('swal-pub-style', GM_getResourceText('Swal'));
+            add_style('swal-pub-style', GM_getResourceText('Swal'));
         }
 
-        addStyle('Panlinker-SweetAlert2-User', swalcss);
+        add_style('Panlinker-SweetAlert2-User', swalcss);
     });
+
     // 再修改主题
     if (unsafeWindow.matchMedia && unsafeWindow.matchMedia('(prefers-color-scheme: dark)').matches) {
         // 切换到暗色主题
-        addStyle('swal-pub-style', GM_getResourceText('SwalDark'));
+        add_style('swal-pub-style', GM_getResourceText('SwalDark'));
     } else {
         // 切换到浅色主题
-        addStyle('swal-pub-style', GM_getResourceText('Swal'));
+        add_style('swal-pub-style', GM_getResourceText('Swal'));
     }
 
-    addStyle('Panlinker-SweetAlert2-User', swalcss);
+    add_style('Panlinker-SweetAlert2-User', swalcss);
 
     // Toast 提示配置
     let toast = Swal.mixin({
         toast: true,
         position: 'top-end',
         showConfirmButton: false,
-        timer: 2000,
+        timer: 3000,
         timerProgressBar: true,
         showCloseButton: true,
         didOpen: function (toast) {
@@ -162,11 +179,11 @@
         }
     };
 
-    function isM3U8File(url) {
+    function is_m3u8_file(url) {
         return /\.m3u8($|\?)/.test(url);
     }
 
-    function extractNumberBeforeTS(str) {
+    function extract_number_before_ts(str) {
         // 匹配 .ts 前面的数字
         const match = str.match(/(\d+)\.ts/);
 
@@ -178,7 +195,7 @@
         return null; // 如果不匹配，返回 null
     }
 
-    function filterLines(lines) {
+    function filter_lines(lines) {
         let result = [];
 
         if (violent_filter_mode_flag) {
@@ -222,7 +239,7 @@
 
                     last_ts_name_len = the_ts_name_len;
 
-                    let ts_name_index = extractNumberBeforeTS(line);
+                    let ts_name_index = extract_number_before_ts(line);
                     if (ts_name_index === null) {
                         if (the_extinf_judge_row_n === 1) {
                             ts_type = 1; // ts命名模式
@@ -235,7 +252,6 @@
                         } else {
                             the_diff_int_ts_n++;
                         }
-
                     } else {
 
                         // 如果序号相隔等于1: 模式0
@@ -303,7 +319,7 @@
                 }
 
                 if (i === lines.length - 1) {
-                    // 后缀不是ts，而是jpeg等等，或者以上规则判断不了的，或者没有广告切片的：直接暴力拆解
+                    // 后缀不是ts，而是jpeg等等，或者以上规则判断不了的，或者没有广告切片的：直接暴力拆解过滤
 
                     ts_type = 2; // ts命名模式
 
@@ -358,7 +374,7 @@
                             }
 
                             // 根据ts序列号过滤
-                            let the_ts_name_index = extractNumberBeforeTS(lines[i + 2]);
+                            let the_ts_name_index = extract_number_before_ts(lines[i + 2]);
 
                             if (the_ts_name_index !== prev_ts_name_index + 1) {
 
@@ -416,7 +432,7 @@
                         }
 
                         // 根据ts序列号过滤
-                        let the_ts_name_index = extractNumberBeforeTS(lines[i + 1]);
+                        let the_ts_name_index = extract_number_before_ts(lines[i + 1]);
 
                         if (the_ts_name_index === prev_ts_name_index + 1) {
 
@@ -522,18 +538,25 @@
         return result;
     }
 
-    async function safelyProcessM3U8(url, content) {
+    async function safely_process_m3u8(url, content) {
         try {
             const lines = content.split('\n');
-            const newLines = filterLines(lines);
+            const new_ines = filter_lines(lines);
 
-            return newLines.join('\n');
+            return new_ines.join('\n');
         } catch (e) {
             filter_log(`处理 m3u8 文件时出错: ${url}`, e);
 
             return content;
         }
     }
+
+    // 脚本菜单变量
+    let menu_item_violent = null;
+    let menu_item_mode = null;
+    let menu_item_host_join = null;
+    let menu_item_toast = null;
+    let menu_item_filter_tip = null;
 
     function hookXHR() {
         const OriginalXHR = unsafeWindow.XMLHttpRequest;
@@ -542,11 +565,12 @@
                 super();
 
                 this.addEventListener('readystatechange', async function () {
-                    if (this.readyState === 4 && this.status === 200 && isM3U8File(this.responseURL)) {
+
+                    if (this.readyState === 4 && this.status === 200 && is_m3u8_file(this.responseURL)) {
 
                         filter_log('----------------------------hookXHR成功---------------------------');
 
-                        const modifiedResponse = await safelyProcessM3U8(this.responseURL, this.responseText);
+                        const modifiedResponse = await safely_process_m3u8(this.responseURL, this.responseText);
                         Object.defineProperty(this, 'responseText', { value: modifiedResponse });
                         Object.defineProperty(this, 'response', { value: modifiedResponse });
 
@@ -554,7 +578,9 @@
                             message.success('已成功过滤切片广告');
                         }
 
-                        GM_registerMenuCommand('提示：已成功过滤视频切片广告');
+                        filter_done_flag = true;
+                        GM_unregisterMenuCommand(menu_item_filter_tip);
+                        check_menu_item_filter_tip();
                     }
                 }, false);
             }
@@ -565,11 +591,7 @@
         hookXHR();
     }
 
-    let menu_item_violent = null;
-    let menu_item_mode = null;
-    let menu_item_host_join = null;
-    let menu_item_toast = null;
-
+    // 初始化菜单判断变量
     violent_filter_mode_flag = GM_getValue('violent_filter_mode_flag', false);
     script_whitelist_mode_flag = GM_getValue('script_whitelist_mode_flag', false);
     the_current_host_in_whitelist_flag = GM_getValue(the_current_host, false);
@@ -675,14 +697,12 @@
                 }
             }
         } else {
-
             if (menu_item_host_join === null) {
                 filter_log('----------------------------脚本加载完成---------------------------');
                 filter_log('----------------------------还没 hookXHR---------------------------');
 
                 initHook();
             }
-
         }
     }
 
@@ -718,6 +738,54 @@
         }
     }
 
+    function register_menu_filter_done_tip() {
+        menu_item_filter_tip = GM_registerMenuCommand('提示：已成功过滤视频切片广告', function() {
+            Swal.fire({
+                title: '过滤日志',
+                type: 'info',
+                icon: 'info',
+                html: filter_log_html,
+                width: '50%',
+                showClass: {
+                    popup: ''
+                },
+                hideClass: {
+                    popup: ''
+                },
+                confirmButtonText: 'OK',
+                showCloseButton: true
+            });
+        });
+    }
+
+    function register_menu_filter_undone_tip() {
+        menu_item_filter_tip = GM_registerMenuCommand('提示：还没有过滤视频切片广告', function() {
+            Swal.fire({
+                type: 'info',
+                icon: 'info',
+                html: '过滤视频切片广告失败！ <br><br> ctrl + F5 刷新试试 <br><br> 如果还不行，那说明：<br><br> 视频格式不是m3u8，无法过滤！',
+                showClass: {
+                    popup: ''
+                },
+                hideClass: {
+                    popup: ''
+                },
+                confirmButtonText: 'OK',
+                showCloseButton: true
+            });
+        });
+    }
+
+    function check_menu_item_filter_tip() {
+        if (filter_done_flag) {
+            register_menu_filter_done_tip();
+        } else {
+            if ((script_whitelist_mode_flag && the_current_host_in_whitelist_flag) || !script_whitelist_mode_flag) {
+                register_menu_filter_undone_tip();
+            }
+        }
+    }
+
     function remove_menu_item_all() {
         GM_unregisterMenuCommand(menu_item_violent);
         GM_unregisterMenuCommand(menu_item_mode);
@@ -735,5 +803,36 @@
     }
 
     add_menu_item_all();
+
+    function listen_video_load_meta() {
+        if ((script_whitelist_mode_flag && the_current_host_in_whitelist_flag) || !script_whitelist_mode_flag) {
+
+            let try_listen_video_meta_n = 0;
+
+            let try_to_add_listen_video_meta = function () {
+                let the_video_dom = unsafeWindow.document.querySelector('video');
+                if (the_video_dom) {
+                    the_video_dom.addEventListener('loadedmetadata', () => {
+                        setTimeout(function() {
+                            if (!filter_done_flag) {
+                                message.warning('过滤失败！ctrl+F5 刷新试试，<br><br> 若还不行，说明播放视频格式<br>不是m3u8，无法过滤！');
+                                GM_unregisterMenuCommand(menu_item_filter_tip);
+                                check_menu_item_filter_tip();
+                            }
+                        }, 1000);
+                    });
+                } else {
+                    try_listen_video_meta_n++;
+                    if (try_listen_video_meta_n < 50) {
+                        setTimeout(try_to_add_listen_video_meta, 100);
+                    }
+                }
+            }
+
+            try_to_add_listen_video_meta();
+        }
+    }
+
+    listen_video_load_meta();
 
 })();
